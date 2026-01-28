7c9906bd6303ff8389c012169c98f22171770b3b
hg8349337-sys <hg8349337@gmail.com>
2026-01-28 10:30:45 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,12 @@
 self.addEventListener('notificationclick', function(event) {
     event.notification.close();
+    const urlToOpen = event.notification.data.url;
     event.waitUntil(
-        clients.matchAll({ type: 'window', includeUncontrolled: true }).then(winClients => {
-            if (winClients.length > 0) return winClients[0].focus();
-            return clients.openWindow('/');
+        clients.matchAll({ type: 'window', includeUncontrolled: true }).then(windowClients => {
+            for (let client of windowClients) {
+                if (client.url === urlToOpen && 'focus' in client) return client.focus();
+            }
+            if (clients.openWindow) return clients.openWindow(urlToOpen);
         })
     );
 });
